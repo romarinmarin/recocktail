936e0beb96a06780cc4d96dccfddad2b19219e8f
Romaric Anquetil <romaric.anquetil@gmail.com>
2020-05-04 16:15:13 +0200

    Error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, est.
+        Fuga odio obcaecati laborum ut saepe ab, vitae accusamus exercitationem
+        incidunt quisquam eaque iusto deserunt, at mollitia quam, consequatur
+        enim!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, est.
+        Fuga odio obcaecati laborum ut saepe ab, vitae accusamus exercitationem
+        incidunt quisquam eaque iusto deserunt, at mollitia quam, consequatur
+        enim!
+      </p>
+    </section>
+  );
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Error() {
-  return <h1>error page</h1>;
+  return (
+    <section className="section error-page">
+      <div className="error-container">
+        <h1>oops wrong way !</h1>
+        <Link className="btn btn-primary" to="/">
+          Back home
+        </Link>
+      </div>
+    </section>
+  );
 }
